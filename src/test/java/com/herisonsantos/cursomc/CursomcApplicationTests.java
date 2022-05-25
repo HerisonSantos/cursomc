9850d829db4db7e81550531ae3a57d4f3b456e9b
HerisonSantos <herisonascimento@gmail.com>
2022-05-25 01:18:59 -0300

    criação do projeto

---
+++ src/test/java/com/herisonsantos/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herisonsantos.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
